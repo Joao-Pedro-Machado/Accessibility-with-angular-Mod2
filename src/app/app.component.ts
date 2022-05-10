9a3f13588d6abcc39e9ee8387f90fe28029d2f70
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-05-10 14:25:17 -0300

    Creating modal component and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ModalRef } from './shared/components/modal/service/modal.service';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { ModalService } from 'src/app/shared/components/modal/service/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'a11y-p2';
+
+  public firstName = 'João Pedro';
+  public modalRef: ModalRef
+
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
+
+  constructor (private modalService: ModalService) {}
+
+  public show() {
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details'
+    })
+  }
 }
